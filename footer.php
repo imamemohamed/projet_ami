481d558956a8440e968f820a4f48f86266900ecb
imamemohamed <145652470+imamemohamed@users.noreply.github.com>
2024-01-18 18:21:11 +0300

    première partie

---
+++ footer.php
@@ -0,0 +1,32 @@
+
+<?php
+    {                 
+        ?>
+         <footer>
+                <div class="footer1">
+                    <div class="tire1">
+                        <div class="titre">
+                            <h5>Arrowwai Industries</h5>
+                        </div>
+
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Blanditiis, fuga. Officia ea deserunt necessitatibus. 
+                        , esse dignissimos nam id sequi officia.</p>
+                </div>
+                <div class="footer2">
+                    <h5>Quick Unix</h5>
+                    <p>Lorem ipsum, dolor sit amet ribus.</p>
+                </div>
+                <div class="footer1">
+                    <h5>Contact Unix</h5>
+                    <p>Lorem ipsum, dolor sit amet consectetur</p>
+                    <p>esse dignissimos nam id sequi officia</p>
+                </div>
+         </footer>
+            <div class="fin">        
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing. Eum, quia deserunt. Est molestiae libero nam!</p>
+            </div>
+        <?php
+    }
+?>
---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include ('head.html');// à rectifier
+    ?>
+    <div class="zut">
+        <div class="zut1">
+            <h1>World Class Interior Design</h1>
+            <p>Lorem ipsum dolor sit amet, connsectetur adipiscing elit,
+             sed do elusmod rempor incididunt ut</p>
+             <h4><a href="*">Get started</a></h4>    
+        </div>
+        <div class="zut2">
+            <img src="" alt="imageplein">
+        </div>
+    </div>
+    <div class="center1">
+        <div class="">
+            <h2>How we works</h2>
+            <p>lorem ipsum dolor sit amet <br>adipiscing elit, sed do</p>
+        </div>
+    </div>
+    <div class="div3">
+        <div class="div31">
+            <img src="image/allo.jpg" alt="premiere iconique">
+            <h3>Planning</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur </p>
+        </div>
+        <div class="div31">
+            <img src="image/allo.jpg" alt="deuxieme iconique">
+            <h3>Bulding</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur </p>
+        </div>
+        <div class="div31">
+            <img src="image/oui.jpeg" alt="troisieme iconique">
+            <h3>Bulding</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur </p>
+        </div>
+    </div>
+    <div class="div3sur4">
+        <div class="im">
+            <img src="" alt="image">
+        </div>
+        <div class="im1">
+            <h2>About Us</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Ea pariatur consequatur neque vel in, 
+                expedita et voluptatem alias ut adipisci beatae cumque aspernatur,
+                 a tempora minus quod officia ad corrupti.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Ea pariatur consequatur neque vel in, 
+                expedita et voluptatem alias ut adipisci beatae cumque aspernatur,
+                 a tempora minus quod officia ad corrupti.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                <h4><a href="*">Learn More</a></h4>
+        </div>
+    </div>
+    <div class="center2">
+        <div class="cent">
+             <h3>Meet Our Experienced Team</h3>
+        </div>
+    </div>
+    <div class="troispetit1">
+        <div class="image1">
+            <img src="" alt="premiere iconique">
+            <h3>Julia Torres</h3>
+            <p>CEO Arrowai</p>
+        </div>
+        <div class="image1">
+            <img src="" alt="deuxieme iconique">
+            <h3>Franco Andrade</h3>
+            <p>Interior Designer</p>
+        </div>
+        <div class="image1">
+            <img src="" alt="troisieme iconique">
+            <h3>Drew Feig</h3>
+            <p>Interior Designer</p>
+        </div>
+    </div> 
+    <div class ="center1">
+      <h2>News & Updates</h2> 
+    </div>
+    <div class="troispetit2">
+     
+    <div class="image1">
+            <img src="" alt="premiere iconique">
+            <h3>Kitchen Design</h3>
+            <p>Lorem ipsum dolor sit amet <br>
+                 consectetur adipisicing </p>
+              <h4><a href="*">Learn More</a></h4>
+        </div>
+        <div class="image1">
+            <img src="" alt="deuxieme iconique">
+            <h3>Family Room</h3>
+            <p>Lorem ipsum dolor sit amet <br>consectetur adipisicing elit.</p>
+                <h4><a href="*">Learn More</a></h4>
+        </div>
+        <div class="image1">
+            <img src="" alt="troisieme iconique">
+            <h3>Light Decoration</h3>
+            <p>Lorem ipsum dolor sit amet <br>consectetur adipisicing elit.</p>
+                <h4><a href="*">Learn More</a></h4>
+        </div>
+    </div>
+    </div>
+    <div class="center3">
+        <div class="">
+            <h3>What they say about us</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, voluptas magni!
+                Repellat culpa, suscipit adipisci , 
+            </p>
+        </div>
+    </div>
+    <div class="center3">
+        <div class="">
+            <h3>Have Any Questions?</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, voluptas magni!
+                Repellat culpa, suscipit adipisci , 
+            </p>
+        </div>
+    </div>
+
+    <?php
+    include ('footer.php');
+    ?>
+</body>
+</html>
